8df7e4bf77aff788c18960745546c798665de9e5
MinnuSudhak <95743800+MinnuSudhak@users.noreply.github.com>
2022-04-25 15:06:57 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -11,27 +11,3 @@ public static void main(String a[])
 }
 
 
-/*import java.util.Scanner;
-class Area
-{
-	double ans;
-	area(){	
-		ans=(22*1*1)/7;
-	}
-	area(double r){	
-		ans= (22*r*r)/7;
-	}
-}
-class Area_of_circle
-{
-   public static void main(String args[]) 	//main method
-    {   
-		area  a = new area();
-		System.out.println("\nArea of Circle is with default Constructor: " + a.ans);  
-		Scanner k= new Scanner(System.in);
-		System.out.print("\nEnter the radius: ");
-		double r = k.nextDouble(); 
-		area  a1 = new area(r);
-		System.out.println("Area of Circle is witht parametrized Constructor: " + a1.ans);      
-   }
- }*/
